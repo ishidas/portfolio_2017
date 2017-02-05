390a0472b64ae10e76d1b25108c3b7f73e68badb
Sawako Ishida <sawako.yoriko@gmail.com>
2017-02-05 13:48:10 -0800

    added fontawesome

--- app/app.js
+++ app/app.js
@@ -1,11 +1,15 @@
 import angular from 'angular';
 require('!style-loader!css-loader!sass-loader!./scss/core.scss');
+require('font-awesome-webpack');
+
 const bgimgUrl = require('./scss/img/mfnbfaciu1i-patrick-schneider.png');
+const bookImgUrl = require('./scss/img/books.jpg');
 
 angular.module('myApp', []).controller('MainController', ['$scope',($scope) => {
   console.log('hitting');
   $scope.test = 'testing';
   $scope.imgUrl = bgimgUrl;
+  $scope.bookImgUrl = bookImgUrl;
 
 }]).controller('scrollController', ['$scope','$location','$anchorScroll', ($scope, $location, $anchorScroll) => {
   $scope.jumpToPageTwo = () => {
@@ -26,10 +30,10 @@ angular.module('myApp', []).controller('MainController', ['$scope',($scope) => {
     restrict: 'E',
     templateUrl: './app/templates/myEdu.html'
   }
-}).directive('myExperienceSection', () => {
+}).directive('mySkillsSection', () => {
   return {
     restrict: 'E',
-    templateUrl: './app/templates/myExperience.html'
+    templateUrl: './app/templates/mySkills.html'
   }
 });
 
--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,14 @@ module.exports = {
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loader: "file-loader?name=[path][name].[ext]"
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&minetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+        loader: "file-loader"
       }
     ]
   }
