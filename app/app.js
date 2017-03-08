8488e77288ba5a4636552db13658f4c47fcc9ff0
sawakoishida <sawako.ishida@tangocard.com>
2017-03-07 21:59:20 -0800

    fixed bugs

--- app/app.js
+++ app/app.js
@@ -1,6 +1,5 @@
 import angular from 'angular';
 import ngRoute from 'angular-route';
-// import ngAnimate from 'angular-animate';
 
 require('!style-loader!css-loader!sass-loader!./scss/core.scss');
 
@@ -19,83 +18,65 @@ App.controller('MainController', ['$scope','$location','$anchorScroll','$window'
   $scope.hamburgerPink = hamburgerPinkNav;
   $scope.hamburgerWhite = hamburgerWhiteNav;
 
-  $scope.navControle = true;
-  $scope.hambergerMenuSwitch = false;
+  $scope.navControle = false;
+  $scope.hambergerMenuSwitch = true;
   $scope.screenWidth = $window.innerWidth;
-  console.log($scope.screenWidth);
-  if ($scope.screenWidth > 481) {
-    $scope.navControle = false;
-  }
-  $scope.navChange = () => {
-    $scope.navControle = $scope.navControle ? false : true;
-    $scope.hambergerMenuSwitch = $scope.hamburgerMenuSwitch ? false : true;
-    console.log($scope.navControle);
-  }
 
-  $scope.jumpToHome = () => {
-    if ($scope.screenWidth > 481) {
+  //watching for screen width change
+  angular.element($window).on('resize', () => {
+    $scope.screenWidth = $window.innerWidth;
+    if($scope.screenWidth > 481) {
       $scope.navControle = false;
     } else {
       $scope.navControle = true;
-      $scope.hambergerMenuSwitch = false;
     }
+  });
 
+  //controles hamburger nav toggle
+  $scope.navChange = () => {
+    $scope.navControle = $scope.navControle ? false : true;
+    $scope.hambergerMenuSwitch = $scope.hamburgerMenuSwitch ? false : true;
+  }
+  
+  $scope.jumpToHome = () => {
+    if($scope.screenWidth > 481) {
+      $scope.navControle = false;
+    } else {
+      $scope.navControle = true;
+    }
     $location.hash('home');
     $anchorScroll();
   }
 
   $scope.jumpToIntro = () => {
-    if ($scope.screenWidth > 481) {
+    if($scope.screenWidth > 481) {
       $scope.navControle = false;
     } else {
       $scope.navControle = true;
-      $scope.hambergerMenuSwitch = false;
     }
     $location.hash('intro');
     $anchorScroll();
   }
 
   $scope.jumpToEducation = () => {
-    if ($scope.screenWidth > 481) {
+    if($scope.screenWidth > 481) {
       $scope.navControle = false;
     } else {
       $scope.navControle = true;
-      $scope.hambergerMenuSwitch = false;
     }
     $location.hash('education');
     $anchorScroll();
   }
 
   $scope.jumpToSkills = () => {
-    if ($scope.screenWidth > 481) {
+    if($scope.screenWidth > 481) {
       $scope.navControle = false;
     } else {
       $scope.navControle = true;
-      $scope.hambergerMenuSwitch = false;
     }
     $location.hash('skills');
     $anchorScroll();
   }
-
-
-
-  // $scope.detectOverlap = () => {
-  //   $scope.dcmt = angular.element($document).find('section').children();
-  //
-  //   $scope.secElements = $scope.dcmt.find('section').children();
-  //   $scope.navElement = $scope.dcmt.find('nav');
-  //   console.log($scope.secElements);
-  //   console.log($scope.navElement);
-  //   $scope.secElTop = $scope.secElements[1].offsetTop;
-  //   $scope.navEle = $scope.navElement[0];
-  //   // let diff = secElTop - navElBottom;
-  //
-  //   console.log($scope.navElBottom);
-  //   if($scope.secElTop <= 60) {
-  //     $scope.navEle.css('color: black');
-  //   }
-  // }
-  // $scope.detectOverlap();
 
 }])
 .controller('scrollController', ['$scope','$location','$anchorScroll', ($scope, $location, $anchorScroll) => {
@@ -125,10 +106,3 @@ App.controller('MainController', ['$scope','$location','$anchorScroll','$window'
     templateUrl: './app/templates/mySkills.html'
   }
 });
-
-// angular.module('myApp').config(['$routeProvider', ($routeProvider) => {
-//   $routeProvider
-//   .when('/', {
-//     templateurl: 'templates/'
-//   })
-// }]);
